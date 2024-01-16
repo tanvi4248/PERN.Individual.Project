1d5064247a03dda60f4ade8b97ed4753363e79aa
tanvi4248 <tanvi@GWTN141-10>
2024-01-16 17:10:42 -0600

    add more endpoints

--- client/src/redux/api.js
+++ client/src/redux/api.js
@@ -3,15 +3,43 @@ import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 const api = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:8080/api"
+        baseUrl: "http://localhost:8080/api",
+        prepareHeaders: (headers, { getState }) => {
+            const { token } = getState();
+            if (token) {
+                headers.set('Authorization', `Bearer ${token}`)
+            }
+            headers.set("Content-Type", "application/json")
+            return headers
+        },
     }),
     endpoints: (builder) => ({
         getTours: builder.query({
             query: () => "/tours",
+        }),
+        getSingleTour: builder.query({
+            query: (tourId) => `/tours/${tourId}`,
+        }),
+        register: builder.mutation({
+            query: ({ firstname, lastname, email, password }) => ({
+                url: '/guests/register',
+                method: "POST",
+                body: { firstname, lastname, email, password },
+            }),
+        }),
+        login: builder.mutation({
+            query: ({ firstname, password }) => ({
+                url: '/guests/login',
+                method: "POST",
+                body: { firstname, password },
+            }),
+        }),
+        getGuest: builder.query({
+            query: (firstname) => `/guests/${firstname}`,
         })
     })
 })
 
 export default api
 
-export const {useGetToursQuery} = api
+export const {useGetToursQuery,useGetSingleTourQuery,useGetGuestQuery,useLoginMutation,useRegisterMutation} = api
