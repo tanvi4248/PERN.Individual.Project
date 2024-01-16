ef5496d5da4b5e06ce6d7e9a25b2039d8233e4e2
tanvi4248 <tanvi@GWTN141-10>
2024-01-16 17:15:52 -0600

    update code

---
+++ server/db/sqlHelperFunctions/guests.js
@@ -0,0 +1,40 @@
+const client = require('../client')
+
+const createGuests = async({firstname,lastname,email,password}) => {
+    try{
+        const {
+            rows: [guests]
+        } = await client.query(`
+            INSERT INTO guests(firstname,lastname,email,password)
+            VALUES($1,$2,$3,$4)
+            RETURNING *;
+            `,[firstname,lastname,email,password]
+            )
+            return guests
+    } catch(error){
+        throw error
+    }
+}
+
+const getAllGuests = async() => {
+    try{
+        const { rows } = await client.query(`
+            SELECT * FROM guests;
+        `)
+        return rows
+    }catch(error){
+        throw error
+    }
+}
+
+
+const getGuestsByFirstname = async(firstname) => {
+    const {
+        rows: [guest],
+    } = await client.query(`
+    SELECT * FROM guests WHERE guests.firstname = '${firstname}'
+    `)
+    return guest
+}
+
+module.exports = {createGuests,getAllGuests,getGuestsByFirstname}
