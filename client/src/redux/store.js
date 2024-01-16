a404b255c73ad8bc52ccc173bd92db0217a5e9bb
tanvi4248 <tanvi@GWTN141-10>
2024-01-16 17:11:07 -0600

    add token reducer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 
 import api from './api';
+import tokenReducer from './tokenSlice'
 
 const store = configureStore({
     reducer: {
         [api.reducerPath]: api.reducer,
+        token: tokenReducer,
     },
     middleware: getDefaultMiddleware => {
         return getDefaultMiddleware().concat(api.middleware);
