4d08d638c4bd2fdc1839e233a5d1b521c9ab2dd1
tanvi4248 <tanvi@GWTN141-10>
2024-01-16 17:09:13 -0600

    add button detail

--- client/src/components/ToursList.jsx
+++ client/src/components/ToursList.jsx
@@ -1,23 +1,28 @@
 import {useGetToursQuery} from '../redux/api'
+import { useNavigate } from "react-router-dom"
 export default function ToursList() {
-  const { data, error, isLoading } = useGetToursQuery();
+  const { data, error, isLoading } = useGetToursQuery()
+  console.log(data)
+  const navigate = useNavigate()
   if (isLoading) {
     return <div>Loading...</div>
   }
 
   if (error || !data) {
-      return <div>Failed to load Books. Try again later.</div>
+      return <div>Failed to load Tours. Try again later.</div>
   }
   return (
     <div className='tour-list'>
       {data.map((tour) => (
-        <div key={tour.id} className='tour-card'>
+        <div key={tour.tourId} className='tour-card'>
             <div className='tour-title'>{tour.title}</div>
             <div className='tour-description'>{tour.description}</div>
             <a className='tour-map' href={tour.googlemap}>Google Map</a>
             <div className='tour-img'>
             <img src={tour.imgUrl} alt={tour.title}></img>
             </div>
+            {tour.id}
+            <button className='btn-detail' onClick={() => navigate(`/tours/${tour.tourId}`)}>See Details</button>
         </div>
       ))}
     </div>
