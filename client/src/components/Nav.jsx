31ab6b7dfe8c34f079fcb95ec0d4dad5a4c53e68
tanvi4248 <tanvi@GWTN141-10>
2024-01-16 17:08:18 -0600

    add login/logout link

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,10 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
+import { setToken } from '../redux/tokenSlice'
+import { useDispatch, useSelector } from 'react-redux'
+
 
 export default function Nav() {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const token = useSelector(state => state.token)
   return (
     <div id="navbar">
         <Link to="/">Home</Link>
-        <Link to="/tours">Tours</Link>       
+        <Link to="/tours">Tours</Link> 
+        {token ? (
+          <>
+            <Link to="/account">Account</Link>
+            <a onClick={() => {
+                                    dispatch(setToken({token: null}))
+                                    navigate('/');
+                                }}>Logout</a> 
+          </>
+        ) : (
+          <>
+            <a onClick={() => navigate('/login')}>Login</a>
+            <a onClick={() => navigate('/register')}>Register</a> 
+          </>
+        )}      
     </div>
   )
 }
