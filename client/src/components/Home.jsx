9bea4b6b26330c75062526ebeb06ebb94aa076d5
tanvi4248 <tanvi@GWTN141-10>
2024-01-19 14:32:26 -0600

    add home page

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,9 @@
+import styles from './Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.home}>
+    <h1>Just go.<br/>Go see all the beauty<br/> in the world.</h1>
+    </div>
+  )
+}
