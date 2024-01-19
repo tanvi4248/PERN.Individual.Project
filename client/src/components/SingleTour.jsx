e6d9153219d6f928a469d31641f4d076059c9e17
tanvi4248 <tanvi@GWTN141-10>
2024-01-19 14:26:48 -0600

    update content

--- client/src/components/SingleTour.jsx
+++ client/src/components/SingleTour.jsx
@@ -1,5 +1,8 @@
 import {useGetSingleTourQuery} from '../redux/api'
 import { useParams } from "react-router-dom"
+import styles from './SingleTour.module.css'
+import Button from 'react-bootstrap/Button';
+
 export default function SingleTour() {
   const {tourId} = useParams()
   const { data: tour, error, isLoading } = useGetSingleTourQuery(tourId)
@@ -12,14 +15,17 @@ export default function SingleTour() {
       return <div>Failed to load Books. Try again later.</div>
   }
   return (
-    <div className='single-tour'>
-      <div className='title'>{tour.title}</div>
-      <div className='img'>
+    <>
+    <h1>Your package</h1>
+    <div className={styles.singletour}>
+      <h2 className={styles.title}>{tour.title}</h2>
+      <div className={styles.img}>
             <img src={tour.imgUrl} alt={tour.title}></img>
             </div>
-      <div className='description'>{tour.description}</div>
-      <a className='map' href={tour.googlemap}>Google Map</a>
- 
+      <div className={styles.description}>{tour.description}</div>
+      <a className={styles.map} href={tour.googlemap}>Google Map</a>
+      <Button variant="primary" className={styles.reserve}>Reserve</Button>
     </div>
+    </>
   )
 }
