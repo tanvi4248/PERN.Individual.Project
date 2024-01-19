7620e25b72b1b75ac9a4d334954fadb80177efb3
tanvi4248 <tanvi@GWTN141-10>
2024-01-19 14:27:50 -0600

    update content

--- client/src/components/ToursList.jsx
+++ client/src/components/ToursList.jsx
@@ -1,5 +1,9 @@
 import {useGetToursQuery} from '../redux/api'
 import { useNavigate } from "react-router-dom"
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import styles from './TourList.module.css'
+import Button from 'react-bootstrap/Button';
 export default function ToursList() {
   const { data, error, isLoading } = useGetToursQuery()
   console.log(data)
@@ -12,19 +16,25 @@ export default function ToursList() {
       return <div>Failed to load Tours. Try again later.</div>
   }
   return (
+    <>
+    <h1>Find your Perfect Vacation</h1>
     <div className='tour-list'>
+      <Row>
       {data.map((tour) => (
-        <div key={tour.tourId} className='tour-card'>
-            <div className='tour-title'>{tour.title}</div>
-            <div className='tour-description'>{tour.description}</div>
-            <a className='tour-map' href={tour.googlemap}>Google Map</a>
-            <div className='tour-img'>
+        <Col key={tour.tourId} xs="4" >
+        <div className={styles.tourcard}>
+            <div className={styles.tourtitle}>{tour.title}</div>
+            <div className={styles.description}>{tour.description}</div>
+            <a className={styles.map} href={tour.googlemap}>Google Map</a>
+            <div className={styles.img}>
             <img src={tour.imgUrl} alt={tour.title}></img>
             </div>
-            {tour.id}
-            <button className='btn-detail' onClick={() => navigate(`/tours/${tour.tourId}`)}>See Details</button>
+            <Button variant="primary" className={styles.detail} onClick={() => navigate(`/tours/${tour.tourId}`)}>See Details</Button>
         </div>
+        </Col>
       ))}
+      </Row>
     </div>
+    </>
   )
 }
