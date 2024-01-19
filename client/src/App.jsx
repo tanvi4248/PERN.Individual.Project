4d8524aeef73d7059f382c3aaf2b8377d7851cd7
tanvi4248 <tanvi@GWTN141-10>
2024-01-19 14:24:45 -0600

    import bootstrap and change home path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,34 @@
 import { Routes, Route } from 'react-router-dom'
-import './App.css'
+import travelLogo from './assets/travellogo.png'
+import styles from './App.module.css'
 import Nav from './components/Nav'
 import ToursList from './components/ToursList'
+import Home from './components/Home'
 import AuthForm from './components/AuthForm'
 import Account from './components/Account'
 import SingleTour from './components/SingleTour'
+import Container from 'react-bootstrap/Container'
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <>
-      <Nav></Nav>
+      <div id='header' className={styles.header}>
+        <div className={styles.headerinner}>
+        <img id='logo-image' src={travelLogo}/>
+        <Nav></Nav>
+        </div>
+      </div>
       <div className='content'>
+      <Container>
       <Routes>
-        <Route to="/" element={<ToursList/>}></Route>
+        <Route path="/" element={<Home/>}/>
         <Route path="/tours" element={<ToursList />} />
         <Route path="/tours/:tourId" element={<SingleTour />}/>
         <Route path="/login" element={<AuthForm type="login" />}/>
         <Route path="/register" element={<AuthForm type="register" />}/>
         <Route path="/account" element={<Account />} />
       </Routes>
+      </Container>
       </div>
     </>
   )
