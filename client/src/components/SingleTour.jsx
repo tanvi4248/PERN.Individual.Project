8daf9493c467523238874b0f685d56faa4b23a0e
tanvi4248 <tanvi@GWTN141-10>
2024-01-19 14:43:57 -0600

    add columns

--- client/src/components/SingleTour.jsx
+++ client/src/components/SingleTour.jsx
@@ -2,7 +2,8 @@ import {useGetSingleTourQuery} from '../redux/api'
 import { useParams } from "react-router-dom"
 import styles from './SingleTour.module.css'
 import Button from 'react-bootstrap/Button';
-
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 export default function SingleTour() {
   const {tourId} = useParams()
   const { data: tour, error, isLoading } = useGetSingleTourQuery(tourId)
@@ -18,13 +19,19 @@ export default function SingleTour() {
     <>
     <h1>Your package</h1>
     <div className={styles.singletour}>
-      <h2 className={styles.title}>{tour.title}</h2>
-      <div className={styles.img}>
-            <img src={tour.imgUrl} alt={tour.title}></img>
-            </div>
-      <div className={styles.description}>{tour.description}</div>
-      <a className={styles.map} href={tour.googlemap}>Google Map</a>
-      <Button variant="primary" className={styles.reserve}>Reserve</Button>
+      <Row>
+      <Col xs={5}>
+        <div className={styles.img}>
+              <img src={tour.imgUrl} alt={tour.title}></img>
+        </div>
+      </Col>
+      <Col xs={7}>
+        <h2 className={styles.title}>{tour.title}</h2>
+        <div className={styles.description}>{tour.description}</div>
+        <a className={styles.map} href={tour.googlemap}>Google Map</a>
+        <Button variant="primary" className={styles.reserve}>Reserve</Button>
+      </Col>
+      </Row>
     </div>
     </>
   )
