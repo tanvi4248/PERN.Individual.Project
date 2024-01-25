e45d94bae755c685b8cac242c6986dcefa10cf23
tanvi4248 <tanvi@GWTN141-10>
2024-01-25 17:08:16 -0600

    add background image for home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,16 @@
 import styles from './Home.module.css'
+import bg from '../assets/bghome.jpg'
 
 export default function Home() {
   return (
-    <div className={styles.home}>
+    <>
+    <div className={styles.bg}>
+      <img id='background-image' src={bg}/>
+      <div className={styles.home}>
     <h1>Just go.<br/>Go see all the beauty<br/> in the world.</h1>
     </div>
+    </div>
+
+    </>
   )
 }
