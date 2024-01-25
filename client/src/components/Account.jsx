4421f784eb526968aa00154049911e26a1c1c859
tanvi4248 <tanvi@GWTN141-10>
2024-01-25 17:06:48 -0600

    set current guest

--- client/src/components/Account.jsx
+++ client/src/components/Account.jsx
@@ -1,27 +1,17 @@
 import { useSelector } from "react-redux"
-import { useGetGuestQuery } from "../redux/api"
 import styles from './Account.module.css'
-
+import { selectCurrentToken, selectCurrentGuest } from "../redux/tokenSlice"
 export default function Account() {
-    const token = useSelector(state => state.token)
-    const {data: user ,error, isLoading } = useGetGuestQuery()
-    console.log(user)
-    if (!token) {
-        return
-    }
-    if (isLoading) {
-        return <div className="loading">Loading...</div>;
-    }
-    
-    if (error || !user) {
-        return <div className="error">Error: {error}</div>;
-    }
-
+  const token = useSelector(selectCurrentToken);
+  const guest = useSelector(selectCurrentGuest);
+  console.log("Token:", token);
+  console.log("User:", guest);
   return (
     <>
+    <h1>Guest Detail</h1>
     <div className={styles.accountdetail}>
-      <h2 className="name">{user.firstname} detail page</h2>
-      <div className="email">{user.email}</div>
+      <h2 className="name">Name: {guest.firstname}</h2>
+      <div className="email">Email: {guest.email}</div>
     </div>
   </>
   )
