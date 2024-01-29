2e051cf62da9f7a12b2f8e2a20fd1e0410753b1c
tanvi4248 <tanvi@GWTN141-10>
2024-01-29 00:16:08 -0600

    Update baseurl

--- client/src/redux/api.js
+++ client/src/redux/api.js
@@ -3,7 +3,7 @@ import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 const api = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:8080/api",
+        baseUrl: "https://tours-0oix.onrender.com/api",
         prepareHeaders: (headers, { getState }) => {
             const { token } = getState().token.token;
             if (token) {
